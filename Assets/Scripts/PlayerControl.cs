252fb26dde47122b605b053d9160c71884247aa1
AshirAli <ashirmayankalathil@gmail.com>
2019-01-26 23:32:57 +0530

    Movement Script Added

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public int speed = 5;
+    public GameObject world;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        world.transform.position += new Vector3(-speed*Time.deltaTime, 0, 0);
+    }
+}
