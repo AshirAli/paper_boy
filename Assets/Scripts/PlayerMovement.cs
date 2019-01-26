ae85479ca987a0781f9e4885cc4a5cfeec8645f2
AshirAli <ashirmayankalathil@gmail.com>
2019-01-27 01:25:25 +0530

    World Create Infinite

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public GameObject world;
-    public Transform prefabParentTransform;
+    //public GameObject world;
+    //public Transform prefabParentTransform;
+
+    public GameObject world1;
+    public GameObject world2;
+
+    private bool world1enter = true, world2enter = false;
+    private float xcord;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //xcord = world.transform.position.x;
     }
 
     // Update is called once per frame
@@ -22,9 +28,22 @@ public class PlayerMovement : MonoBehaviour
     {
         if (other.gameObject.tag == "End_Trigger")
         {
+            if (world1enter)
+            {
+                world1.transform.position += world2.transform.position + new Vector3(550, 0, 0);
+                world1enter = false;
+                world2enter = true;
+            }
+            if (world2enter)
+            {
+                world2.transform.position += world1.transform.position + new Vector3(550, 0, 0);
+                world2enter = false;
+                world1enter = true;
+            }
             Debug.Log("New world create");
-            GameObject instance = (GameObject)Instantiate(world, prefabParentTransform) as GameObject;
-            instance.transform.position = new Vector3(100, 0, 0);
+
+            //GameObject instance = (GameObject)Instantiate(world, prefabParentTransform) as GameObject;
+            //instance.transform.position = new Vector3(xcord+347, 0, 0);
         }
     }
 }
