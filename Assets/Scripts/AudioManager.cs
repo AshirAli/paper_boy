82d8ce66661a9d50d6145724f92dc9abfc617a89
AshirAli <ashirmayankalathil@gmail.com>
2019-01-27 09:42:34 +0530

    SteamVR Plugin

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public Sound[] sounds;
+    public static AudioManager instance;
+
+    void Awake()
+    {
+
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.loop = s.loop;
+            s.source.volume = s.volume;
+        }
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+        s.source.Play();
+    }
+    public void Pause(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+        s.source.Pause();
+    }
+
+}
---
+++ Assets/Sound.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+
+    public string name;
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume;
+
+    public bool loop;
+
+    [HideInInspector]
+    public AudioSource source;
+
+}
